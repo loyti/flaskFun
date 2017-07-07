34cd88d5fcc69ef5daa59d69ae598c6c05d04b66
loyti <brice@loyti.com>
2017-07-06 20:21:49 -0700

    initial commit

--- flaskFun/ninja/server.py
+++ flaskFun/ninja/server.py
@@ -22,4 +22,28 @@ def process():
 	print email
 	return render_template('process.html',name=name, email=email)
 
+@app.route('/turtle')
+def turtle():
+	return render_template('turtle.html')
+
+@app.route('/blue')
+def blue():
+	return render_template('blue.html')
+
+@app.route('/red')
+def red():
+	return render_template('red.html')
+
+@app.route('/orange')
+def orange():
+	return render_template('orange.html')
+
+@app.route('/purple')
+def purple():
+	return render_template('purple.html')
+
+@app.route('/*')
+def april():
+	return render_template('april.html')
+
 app.run(debug=True)
