0b569757c21a6c2f389f0e879ac1b7efec903761
loyti <brice@loyti.com>
2017-07-07 12:48:14 -0700

    updated server & index files

--- flaskFun/portfolio/server.py
+++ flaskFun/portfolio/server.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
+import random
 
 app = Flask(__name__)
-
+app.secret_key = "secretKey"
 @app.route('/')
 def index():
-	return render_template('index.html')
+	session["win"] = session.get("win", 0)
+	session["loss"] = session.get("loss", 0)
+	session["tie"] = session.get("tie", 0)
+	return render_template("index.html")
 
 @app.route('/about')
 def about():
@@ -13,6 +17,45 @@ def about():
 @app.route('/projects')
 def projects():
 	return render_template('projects.html')
+@app.route("/process_play", methods=["POST"])
+def process_play():
+    opponentHand = random.randint(0,2)
+    arrayHand = ["rock", "paper", "scissors"]
+    opponent = arrayHand[opponentHand]
+    session["opponentOut"] = opponent
+    session["playerHand"] = request.form["hand"]
+    if session["playerHand"] == opponent:
+        session["tie"] += 1
+        session["outcome"] = "tie"
+    elif session["playerHand"] == "rock":
+        if opponent == "paper":
+            session["loss"] += 1
+            session["outcome"] = "loss"
+        elif opponent == "scissors":
+            session["win"] += 1
+            session["outcome"] = "win"
+    elif session["playerHand"] == "paper":
+        if opponent == "rock":
+            session["win"] += 1
+            session["outcome"] = "win"
+        elif opponent == "scissors":
+            session["loss"] += 1
+            session["outcome"] = "loss"
+    elif session["playerHand"] == "scissors":
+        if opponent == "rock":
+            session["loss"] += 1
+            session["outcome"] = "loss"
+        elif opponent == "paper":
+            session["win"] += 1
+            session["outcome"] = "win"
+    return redirect("/")
+
+@app.route("/endGame", methods=["POST"])
+def endGame():
+    session.pop("win")
+    session.pop("loss")
+    session.pop("tie")
+    return redirect("/")
 
 @app.route('/survey', methods=['POST'])
 def survey():
